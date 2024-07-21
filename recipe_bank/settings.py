ad28a2ba2d7e11d0e55eccd727d028db66bf267c
Karl Robert Nilsson <krnilsson@yandex.com>
2024-07-21 13:10:19 +0000

    Add recipe detail view and link it from the list view template

--- recipe_bank/settings.py
+++ recipe_bank/settings.py
@@ -49,6 +49,7 @@ CSRF_TRUSTED_ORIGINS=['https://8000-krnils-recipebank-q5jrb92lnke.ws.codeinstitu
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- recipes/views.py
+++ recipes/views.py
@@ -3,8 +3,18 @@ from django.views import generic
 from .models import Recipe
 
 # Create your views here.
+
+
 class RecipeList(generic.ListView):
     model = Recipe
     template_name = "recipes/index.html"
     queryset = Recipe.objects.all()
     paginate_by = 2
+
+
+class RecipeDetail(generic.DetailView):
+    model = Recipe
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
